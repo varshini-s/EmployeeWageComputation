c84e8b2a624caacb2099850e7b16f847b4abc111
varshini <dreamvarshini@gmail.com>
2021-10-05 18:38:01 +0530

    MODIFIED: calculating wage for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,37 +1,29 @@
-{
-    const IS_ABSENT = 0
-
-    let empCheck = Math.floor(Math.random() * 10) % 2;
-    if (empCheck == IS_ABSENT)
-    {
-        console.log("Employee is Absent");
-        return
-    }
-    else
-    {
-        console.log("Employee is Present");
-    }
-}
-
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HOURS=4;
 const FULL_TIME_HOURS=8;
 const WAGE_PER_HOUR=20;
-
-let employeeHours=0;
-let employeeCheck=Math.floor(Math.random()*10)%3;
-switch(employeeCheck)
+const NUMBER_OF_WORKING_DAYS=20;
+function getWorkingHours(employeeCheck)
 {
-    case IS_PART_TIME:
-        employeeHours=PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        employeeHours=FULL_TIME_HOURS;
-        break;
-    default:
-        employeeHours=0;
+
+    switch (employeeCheck)
+     {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
 
-let employeeWage=employeeHours*WAGE_PER_HOUR;
-console.log("Employee wage: "+employeeWage);
+let totalEmployeeHours=0;
+for(let day=0;day<NUMBER_OF_WORKING_DAYS;day++)
+{
+    let employeeCheck=Math.floor(Math.random()*10)%3;
+    totalEmployeeHours+=getWorkingHours(employeeCheck);
+}
+
+let employeeWage=totalEmployeeHours*WAGE_PER_HOUR;
+console.log("Employee hours "+totalEmployeeHours+" Employee wage: "+employeeWage);
