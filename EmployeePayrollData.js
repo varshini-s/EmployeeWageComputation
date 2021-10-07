f46b3f9ac12f91c9507813f4722ef7f83703bb07
varshini <dreamvarshini@gmail.com>
2021-10-07 14:52:17 +0530

    REFACTORED: gender and date field added

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -2,12 +2,17 @@ class EmployeePayrollData
 {
     id;
     salary;
+    gender;
+    startDate;
 
-    constructor(id,name,salary)
+    constructor(...params) 
     {
-        this.id=id;
-        this.name=name;
-        this.salary=salary;
+        this.id=params[0];
+        this.name=params[1];
+        this.salary=params[2];
+        this.gender=params[3];
+        this.startDate=params[4];
+        
     }
     
     //getter and setters
@@ -23,12 +28,19 @@ class EmployeePayrollData
 
     toString()
     {
-      return "id="+this.id+",name='"+this.name+",salary="+this.salary;  
+        const options={year:'numeric',month:'long',day:'numeric'};
+        const employeeDate=this.startDate==undefined?"undefined":
+                           this.startDate.toLocaleDateString("en-us",options);
+      return "id="+this.id+",name='"+this.name+",salary="+this.salary+
+              ",gender= "+this.gender+",startDate= "+employeeDate;  
     }
 }
 let employeePayrollData = new EmployeePayrollData(1, "Mark", 30000);
 console.log(employeePayrollData.toString());
 employeePayrollData.name = "John"
 console.log(employeePayrollData.toString());
+let newEmployeePayrollData= new EmployeePayrollData(1,"Terisa",30000,"F",new Date());
+console.log(newEmployeePayrollData.toString());
+
 
     
